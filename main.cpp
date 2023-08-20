fcf32a70fd067af89897ab980b9cc917ad66e14f
David Hawickhorst <davidhawick@gmail.com>
2023-08-20 14:37:54 -0500

    basic setup to test build

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    sf::CircleShape circle(50.f);
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition(100, 200);
+
+    sf::CircleShape triangle(50.f, 3);
+    triangle.setFillColor(sf::Color::Blue);
+    triangle.setPosition(475, 375);
+
+    // Start the game loop
+    while (window.isOpen()) {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(circle);
+        window.draw(triangle);
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
